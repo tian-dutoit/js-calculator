0cb3c47e5798e84adbae5d73bff9054a2ca44402
Tian <Tian@Louises-iMac-2.local>
2017-12-28 20:29:12 +1300

    adding html and css  skeleton for calculator

---
+++ calculator.js
@@ -0,0 +1,61 @@
+
+//taking the input from the button and displaying it in the display area
+$("clear").click(reset(){
+
+})
+
+function reset(){
+
+}
+
+
+
+// //taking the input and adding to the current value
+//
+// Function calculator(selectionOne, selectionTwo){
+//
+// }
+//
+//
+//
+// //selecting the operator
+//
+//
+//
+// //looping through and accepting additional values and operators
+//
+//
+//
+// //equals running the equation
+//
+//
+//
+//
+//
+// //clearing the display
+//
+// $(‘#clear’).click(reset(){});
+//
+//
+// function reset(){
+//
+// $(‘#display’).innerHTML(‘0’)
+//
+// }
+//
+//
+// // +/- changing the sign
+//
+// $(‘#sign’).click(changeSign(){});
+//
+//
+// function changeSign(){
+//
+//   var value = $(‘#display’).value; //get current display value
+//
+//   var newValue = value * -1;
+//
+
+// }
+//
+//
