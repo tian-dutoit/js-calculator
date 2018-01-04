de9e6b2b19786f2f0066babca4b42b3def36227d
Tian <Tian@Louises-iMac-2.local>
2018-01-05 11:22:46 +1300

    added divs for testing values and updated caluclation to monitor current value and result

--- calculator.js
+++ calculator.js
@@ -1,40 +1,78 @@
 $(document).ready(function(){
-//taking the input from the button and displaying it in the display area
+var result = 0;
+var second = 0;
+var operator = 0;
+var currentVal = 0;
+///////////event handlers for operators
+($("#plus")).click(function(){
+  operator = 0;
+  currentVal = parseInt($("#screen").html());
+  result = result + currentVal;
+  $("#screen").html("");
+});
 
-var result = parseInt($('#screen').html());
-//console.log(result);
-var first = 10;
-var second = 2;
-var operator = 9;
+($("#minus")).click(function(){
+  operator = 1;
+  currentVal = parseInt($("#screen").html());
+  $("#screen").html("");
+});
 
+($("#times")).click(function(){
+  operator = 2;
+  currentVal = parseInt($("#screen").html());
+  $("#screen").html("");
+});
+
+($("#divide")).click(function(){
+  operator = 3;
+  currentVal = parseInt($("#screen").html());
+  $("#screen").html("");
+});
 
 //append the button pressed to current display
-$("#test").click(function(){
-  $("#screen").append("t");
-  result = $('#screen').html();
-//  console.log(result);
-});
+// $('.numb').click(function(){
+//   $("#screen").append($(this).html());
+//   currentVal = parseInt($('#screen').html());
+// //  alert(currentVal);
+// });
+
+
+////////////////////////////////////
+//Appending selection to show current equation
+var selection = "";
+
+var selectString = function(){
+  $("#test").append(($(this).html()));
+};
+
+$('button').click(selectString);
 
 
 //clearing the screen on C press
 $("#zero").click(function(){
   $("#screen").html("0");
+  $("#test").html("");
+  currentVal = 0;
+  second = 0;
+  result = 0;
 });
 
-// clicking on equals runs the calculation
+
+//clicking on equals runs the calculation
 $("#equals").click(function(){
-  //var welp = parseInt(first) + parseInt(second);
+  currentVal = parseInt($("#screen").html());
+  $("#test").html("");
   if(operator === 0){
-  addition(first, second);
+    addition(result, currentVal);
   }
   else if(operator === 1){
-    subtraction(first, second);
+    subtraction(currentVal, second);
   }
   else if(operator === 2){
-    multiply(first, second);
+    multiply(currentVal, second);
   }
   else if(operator === 3){
-    division(first, second);
+    division(currentVal, second);
   }
   else{
     alert("Error");
@@ -42,34 +80,47 @@ $("#equals").click(function(){
   }
 
 });
-//   var welp = first + second;
-//   alert(welp);
-// });
 
-var addition = function(first, second) {
-   var calc = first+second;
-   alert(calc)
-   $("#screen").html(calc);
+
+
+//*functions*
+
+//append the button pressed to current display
+
+var app = function(){
+  $("#screen").append($(this).html());
 };
 
-var subtraction = function(first, second) {
-  var calc = first-second;
-  alert(calc)
-  $("#screen").html(calc);
+$('.numb').click(app);
+
+//calculation functions
+var addition = function(result, currentVal) {
+   result = result+currentVal;
+   $("#screen").html(result);
+   result = 0;
 };
 
-var multiply = function(first, second) {
-   var calc = first*second;
-   alert(calc)
-   $("#screen").html(calc);
+var subtraction = function(currentVal, second) {
+  var currentVal = currentVal-second;
+  $("#screen").html(currentVal);
 };
 
-var division = function(first, second) {
-  var calc = first/second;
-  alert(calc)
-  $("#screen").html(calc);
+var multiply = function(currentVal, second) {
+   var currentVal = currentVal*second;
+   $("#screen").html(currentVal);
 };
 
+var division = function(currentVal, second) {
+  var currentVal = currentVal/second;
+  $("#screen").html(currentVal);
+};
+
+$('button').click(function(){
+  $("#current").html(currentVal);
+  $("#sec").html(second);
+  $("#res").html(result);
+
+});
 
 
 });
